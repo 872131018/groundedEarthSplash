2c71c0a9ec5f503365381e8b46824836f91b70f9
872131018 <j-huffman@hotmail.com>
2016-02-24 21:19:39 -0700

    changes to ajax url

--- public/js/splashDriver.js
+++ public/js/splashDriver.js
@@ -10,7 +10,7 @@ $(document).ready()
 		*/
     if(validateSignup())
 		{
-      var url = window.location.href+"index.php/signup";
+      var url = window.location.href+"/signup";
       $.post(url, $("form").serialize(), function(response, status)
     	{
     		if(status == "success")
